4927327fb42e4561da47892406f887ba5a9e7b8f
George White <stonehippo@gmail.com>
2015-05-02 17:14:50 -0400

    API prototype for basic movement

--- tankbot.ino
+++ tankbot.ino
@@ -1,4 +1,6 @@
 #include <FiniteStateMachine.h>
+#include <SPI.h>
+#include <aREST.h>
 #include "LogHelpers.h"
 #include "TimingHelpers.h"
 
@@ -8,15 +10,20 @@
 // FSM states
 State startupState = State(enterStartupState,updateStartupState,leaveStartupState);
 State idleState = State(enterIdleState,updateIdleState,leaveIdleState);
+State forwardState = State(enterForwardState, updateForwardState, leaveForwardState);
+State backwardState = State(enterBackwardState, updateBackwardState, leaveBackwardState);
 
-FSM stateMachine = FSM(startupState);
+FSM stateMachine = FSM(startupState);  
+
+aREST rest = aREST();
 
 void setup() {
-  startLog();
+  startLog(); // starts Serial, and prints a message if DEBUG is enabled
 }
 
 void loop() {
   stateMachine.update();
+  rest.handle(Serial);
 }
 
 // ******************* FSM state callback methods *******************
@@ -25,6 +32,13 @@ void enterStartupState() {
   info(F("Booting up..."));
 }
 void updateStartupState() {
+  // set up the REST listener
+  rest.function("tb", setControlState);
+  
+  // get the REST service registered
+  rest.set_id("001");
+  rest.set_name("tankbot");
+
   stateMachine.transitionTo(idleState);
 }
 void leaveStartupState() {
@@ -34,13 +48,72 @@ void leaveStartupState() {
 // -------------- Idle State ---------------
 void enterIdleState() {
   info(F("Idling, ready for commands..."));
-  digitalWrite(13, HIGH);
+  allStop();
 }
-void updateIdleState() {
+void updateIdleState() {}
+void leaveIdleState() {}
 
+// -------------- Forward State ---------------
+
+// Move the bot forward at full speed
+void enterForwardState() {
+  allStart();
+  allSpeedAhead();
 }
-void leaveIdleState() {
+void updateForwardState() {}
+void leaveForwardState() {
+  allStop();
+}
 
+// -------------- Backward State ---------------
+
+// Move the bot backward at full speed
+void enterBackwardState() {
+  allStart();
+  allSpeedReverse();
+}
+void updateBackwardState() {}
+void leaveBackwardState() {
+  allStop();
+}
+
+
+// ******************* HELPERS *******************
+
+// aREST API listener
+int setControlState(String command) {
+  info(F("Handling a REST request"));
+  info(command);
+  if (command.startsWith("stop")) {
+     stateMachine.transitionTo(idleState);
+     return 2;
+  }
+  if (command.startsWith("forward")) {
+     stateMachine.transitionTo(forwardState);
+     return 3;
+  }
+  if (command.startsWith("backward")) {
+     stateMachine.transitionTo(backwardState);
+     return 4;
+  }
+  return 1; 
 }
 
 // ******************* HELPERS *******************
+
+// -------------- Motor Driver Helpers ---------------
+// disable the motors via the motor driver STANDBY
+void allStop() {
+  warn(F("enabling STANDBY"));
+}
+// disable the motors via the motor driver STANDBY
+void allStart() {
+  warn(F("disabling STANDBY"));
+}
+void allSpeedAhead() {
+  warn(F("moving forward"));
+}
+void allSpeedReverse() {
+  warn(F("moving backward"));
+}
+
